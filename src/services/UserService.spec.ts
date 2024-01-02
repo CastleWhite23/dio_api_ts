fb7069e131f21ebf0e99ebcac1f80c52cd360582
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-02 14:04:52 -0300

    Adicionando mockDb no arquivo de teste

--- src/services/UserService.spec.ts
+++ src/services/UserService.spec.ts
@@ -1,15 +1,15 @@
 import { UserService } from "./UserService"
-
+import { User } from "./UserService"
 describe("UserService", ()=>{
-
-    const userService =  new UserService()
+    const mockDb: User[] = []
+    const userService =  new UserService(mockDb) //aqui esta sendo mockado o banco de dados para nao ter acesso as informacoes do banco de dados
     it('Receber um console.log', ()=>{
         
-
+       
         const mockConsole = jest.spyOn(global.console, "log")
 
         userService.createUser("pedro", "pedro@email")
-        
+
         expect(mockConsole).toHaveBeenCalled()
     })
 })
