7e634285f3a4ee5bc5921c833b020862d6b1eae5
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 10:24:52 -0300

    Fazendo o teste funcionar

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -2,16 +2,27 @@ import { makeMockRequest } from "../__mocks__/mockRequest.mock"
 import { makeMockResponse } from "../__mocks__/mockResponse.mock"
 import { UserService } from "../services/UserService"
 import { UserController } from "./UserController"
+import { Request } from "express"
 
 describe('UserController', ()=>{
-    const mockUserService: Partial<UserService> = {}//. O uso de Partial<T> é uma característica do TypeScript que indica que não é necessário fornecer todas as propriedades definidas em UserService no objeto mockUserService.
+    const mockUserService: Partial<UserService> = {
+        createUser: jest.fn()
+
+    }//. O uso de Partial<T> é uma característica do TypeScript que indica que não é necessário fornecer todas as propriedades definidas em UserService no objeto mockUserService.
 
     const userController = new UserController(mockUserService as UserService)
 
     it('Deve adicionar um novo usuario', () => {
-        const mockRequest = makeMockRequest({});
+        const mockRequest = {
+            body: {
+                name: "pedro",
+                email: "pedro@email"
+            }
+        } as Request;
         const mockResponse = makeMockResponse();
         const response = userController.createUser(mockRequest, mockResponse);
+
+        console.log(response)
     });
 
 
