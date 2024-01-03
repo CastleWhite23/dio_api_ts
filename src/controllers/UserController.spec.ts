842ebbd6b935c8c5c262b0c41ae8a086e76bdd40
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 10:12:34 -0300

    importando mock Response no arquivo de teste

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -1,43 +1,13 @@
 import { makeMockRequest } from "../__mocks__/mockRequest.mock"
+import { makeMockResponse } from "../__mocks__/mockResponse.mock"
 import { UserService } from "../services/UserService"
 import { UserController } from "./UserController"
-import { Response } from "express"
-
-type MockResponse<Tresult> = Response & {
-    state: {
-        status?: number,
-        json?: Tresult | unknown
-    }
-}
-
 
 describe('UserController', ()=>{
     const mockUserService: Partial<UserService> = {}//. O uso de Partial<T> é uma característica do TypeScript que indica que não é necessário fornecer todas as propriedades definidas em UserService no objeto mockUserService.
 
     const userController = new UserController(mockUserService as UserService)
 
-    
-    
-    function makeMockResponse <TResult> (){
-        const response = {
-            state: {}
-        } as MockResponse<TResult>
-
-        response.status = (status: number) =>{
-            response.state.status = status
-            return response
-        }
-
-        response.json = (json: TResult) =>{
-            response.state.json = json
-            return response
-        }
-
-        return response
-
-    }
-
-    
     it('Deve adicionar um novo usuario', () => {
         const mockRequest = makeMockRequest({});
         const mockResponse = makeMockResponse();
