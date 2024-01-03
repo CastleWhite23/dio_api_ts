894859e8c0aa11abe8920e7bb58f0d7b517b9578
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 10:09:00 -0300

    Adicioando código no mock request e exportando ele

--- src/__mocks__/mockRequest.mock.ts
+++ src/__mocks__/mockRequest.mock.ts
@@ -1,7 +1,8 @@
 import { Params } from 'express-serve-static-core'
 import { Request } from "express"
 
-const makeMockRequest = ({params, query}: {params?: Params, query?: Params}) : Request => {
+
+export const makeMockRequest = ({params, query}: {params?: Params, query?: Params}) : Request => {
     const request = {
         params: params || {},
         query: query || {}
