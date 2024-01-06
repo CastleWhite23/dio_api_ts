81cd98bb9316d110efebf4a64391f7f10e993fbc
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-06 10:50:28 -0300

    Adicionando comando para criacao e remocao da tabel users

--- src/database/migrations/1704547893778-User.ts
+++ src/database/migrations/1704547893778-User.ts
@@ -1,11 +1,24 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class User1704547893778 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.createTable(
+            new Table({ //objeto da tabela
+                name: "users",
+                columns: [//array das colunas
+                    {//objeto de cadacoluna
+                        name: "id_user",
+                        type: "string",
+                        isPrimary: true
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable("users")
     }
 
 }
