d8650c07115517ce0301017106b478ce5c1fcaf8
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-06 10:14:53 -0300

    recortando promise

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,11 +8,3 @@ export const AppDataSource = new DataSource({
         "./src/database/migrations"
     ]
 })
-
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
