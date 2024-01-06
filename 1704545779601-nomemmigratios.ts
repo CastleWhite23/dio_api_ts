74cb0cf29d637d8686b9a2988f4935cbad8dc89e
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-06 09:59:17 -0300

    arquivo teste de migrations criado

---
+++ 1704545779601-nomemmigratios.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Nomemmigratios1704545779601 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
