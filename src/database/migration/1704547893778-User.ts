af4d74e73bce43ee206818c4dbbddeb0b242124d
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-06 10:32:17 -0300

    adicionado migrations users

---
+++ src/database/migration/1704547893778-User.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class User1704547893778 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
