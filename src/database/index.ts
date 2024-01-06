479ba0c08dc393dfedeba61428dd9265ea168070
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-06 10:13:17 -0300

    Adicionado arquivo de condifhuracao atualizado

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    //configracoes relacionanas ao banco de dados
+    type: "sqlite",
+    database: ".src//database/db.sqlite",
+    migrations: [
+        "./src/database/migrations"
+    ]
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
