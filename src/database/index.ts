3ace746160fbe6ab8582830928c93753daad488d
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-06 10:57:41 -0300

    Configurando index.ts para query funcionar

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,9 +3,9 @@ import { DataSource } from "typeorm"
 export const AppDataSource = new DataSource({
     //configracoes relacionanas ao banco de dados
     type: "sqlite",
-    database: ".src//database/db.sqlite",
+    database: "./src/database/db.sqlite",
     migrations: [
-        "./src/database/migrations"
+        "./src/database/migrations/*.ts"
     ]
 })
 
