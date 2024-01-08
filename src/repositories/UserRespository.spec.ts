29685fc45c84d3b8b5b5cd79a79f635c706b001f
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 09:41:55 -0300

    Criando o mock da classe userREspository e indo testar a a func create user

--- src/repositories/UserRespository.spec.ts
+++ src/repositories/UserRespository.spec.ts
@@ -1,8 +1,17 @@
+import { getMockEntityManager } from "../__mocks__/mockEntityManager.mock"
+import { UserRepository } from "./UserRepository"
+
 describe('UserRepository', ()=>{
 
-    let userRepository
-    it('Deve criar um novo usuario no banco de dados', ()=>{
+    let userRepository: UserRepository
 
+    beforeAll( async()=>{
+        const managerMock =  await getMockEntityManager({})
+        const userRepository = new UserRepository(managerMock)
+    })
+
+    it('Deve criar um novo usuario no banco de dados', async ()=>{
+         await userRepository.createUser()
     })
 
 })
