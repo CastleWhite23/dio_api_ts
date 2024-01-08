09f5e6d4df34cbb374601b6b9143786f5f01cecb
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 08:37:45 -0300

    Adicionando contructor e metodo para criar usuario na class UserRepository

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,22 @@
+import { EntityManager } from "typeorm";
+import { AppDataSource } from "../database";
+import { User } from "../entities/User";
+
+
+
+
+export class UserRepository{
+    private manager: EntityManager
+
+    constructor(
+        manager = AppDataSource.manager
+    ){
+        this.manager=manager
+    }
+    
+    createUser = async (user: User) =>{
+        return this.manager.save(user)
+    }
+
+
+}
