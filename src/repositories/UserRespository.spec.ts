62f0df9b37658ac6e6c4b8ed4af167ab3386896e
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 09:48:18 -0300

    Testando pra ver se a func save foi chamada

--- src/repositories/UserRespository.spec.ts
+++ src/repositories/UserRespository.spec.ts
@@ -1,3 +1,4 @@
+import { EntityManager } from "typeorm"
 import { getMockEntityManager } from "../__mocks__/mockEntityManager.mock"
 import { User } from "../entities/User"
 import { UserRepository } from "./UserRepository"
@@ -5,6 +6,7 @@ import { UserRepository } from "./UserRepository"
 describe('UserRepository', ()=>{
 
     let userRepository: UserRepository
+    let managerMock: Partial<EntityManager>
 
     const mockUser: User = {
         user_id: '123456',
@@ -14,12 +16,14 @@ describe('UserRepository', ()=>{
     }
 
     beforeAll( async()=>{
-        const managerMock =  await getMockEntityManager({})
-        const userRepository = new UserRepository(managerMock)
+        managerMock =  await getMockEntityManager({})
+        const userRepository = new UserRepository(managerMock as EntityManager)
     })
 
     it('Deve criar um novo usuario no banco de dados', async ()=>{
          await userRepository.createUser(mockUser)
+
+         expect(managerMock.save).toHaveBeenCalled()
     })
 
 })
