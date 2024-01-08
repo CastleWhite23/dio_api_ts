050991fafb13eddb883e1762dc34534ce1009583
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 09:16:53 -0300

    Alterando o tipo do parametro no metodo constructor

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -3,13 +3,11 @@ import { AppDataSource } from "../database";
 import { User } from "../entities/User";
 
 
-
-
 export class UserRepository{
     private manager: EntityManager
 
     constructor(
-        manager = AppDataSource.manager
+        manager: EntityManager
     ){
         this.manager=manager
     }
