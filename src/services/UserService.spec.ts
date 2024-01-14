c3626c03e6f07907d2eb5d396fceae0bde6963ab
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-14 12:24:54 -0300

    elaborando melhor o teste

--- src/services/UserService.spec.ts
+++ src/services/UserService.spec.ts
@@ -1,17 +1,35 @@
-import { User, UserService } from "./UserService";
+import { UserRepository } from "../repositories/UserRepository";
+import {UserService } from "./UserService";
+jest.mock("../repositories/UserRepository")
+const mockRepository =  require("../repositories/UserRepository")
 
 describe('UserService', () => {
-    const mockDb: User[] = []
-    const userService = new UserService(mockDb);
+    const userService =  new UserService(mockRepository)
+    it('Deve adicionar um novo usuário', async () => {
+        mockRepository.createUser =  jest.fn().mockImplementation(() =>
+            Promise.resolve({
+                id_user: '123456',
+                name: 'nath',
+                email: 'nath@test.com',
+                password: 'senha'
+            })
+        )
 
-    it('Deve adicionar um novo usuário', () => {
-        const mockConsole = jest.spyOn(global.console, 'log')
-        userService.createUser('nath', 'nath@test.com');
-        expect(mockConsole).toHaveBeenCalledWith('DB atualizado', mockDb)
+        const response = await userService.createUser('nath', 'nath@test.com', 'senha');
+        expect(mockRepository.createUser).toHaveBeenCalled()
+        expect(response).toMatchObject({
+            id_user: '123456',
+            name: 'nath',
+            email: 'nath@test.com',
+            password: 'senha'
+        })
+
+        
+    
     })
-    it('Deve excluir um usuário', () => {
-        const mockConsole = jest.spyOn(global.console, 'log')
-        userService.delete();
-        expect(mockConsole).toHaveBeenCalledWith('DB atualizado')
-    })
+    // it('Deve excluir um usuário', () => {
+    //     const mockConsole = jest.spyOn(global.console, 'log')
+    //     userService.delete();
+    //     expect(mockConsole).toHaveBeenCalledWith('DB atualizado')
+    // })
 })
