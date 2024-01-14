d7b08704fb3910840e7e5afea763236353c40fe8
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-14 12:25:20 -0300

    adicionando findOneReturn como mock

--- src/__mocks__/mockEntityManager.mock.ts
+++ src/__mocks__/mockEntityManager.mock.ts
@@ -1,15 +1,18 @@
 import { EntityManager } from "typeorm";
 
 interface MockManagerArgs {
-    saveReturn?: object | [object]
+    saveReturn?: object | [object],
+    findOneReturn?: object | null
 }
 
 export const getMockEntityManager = async ({
-    saveReturn = undefined
+    saveReturn = undefined,
+    findOneReturn = undefined
 }: MockManagerArgs): Promise<EntityManager> => {
     const manager: Partial<EntityManager> = {}
 
     manager.save = jest.fn().mockImplementation(() => Promise.resolve(saveReturn))
+    manager.findOne = jest.fn().mockImplementation(() => Promise.resolve(findOneReturn))
 
     return manager as EntityManager
 }
