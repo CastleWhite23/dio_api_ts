05f640faac768364d51541b59ad5c584b68b2d36
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-14 12:24:24 -0300

    atualizando metodo createuser

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,41 +1,27 @@
-export interface User {
-    name: string
-    email: string
-}
-
-const db = [
-    {
-        name: "Joana",
-        email: "joana@dio.com",
-    }
-]
+import { AppDataSource } from "../database"
+import { User } from "../entities/User";
+import { UserRepository } from "../repositories/UserRepository"
 
 export class UserService {
-    db: User[]
+    private userRepository: UserRepository;
 
     constructor(
-        database = db
+        userRepository = new UserRepository(AppDataSource.manager)
     ){
-        this.db = database
+        this.userRepository = userRepository
     }
 
-    createUser = (name: string, email: string) => {
-        const user = {
-            name,
-            email
-        }
-
-        this.db.push(user)
-        console.log('DB atualizado', this.db)
+    createUser = async (name: string, email: string, password: string): Promise<User> => {
+        const user =  new User(name, email, password)
+        return this.userRepository.createUser(user)
     }
 
-    getAllUsers = () => {
-        return this.db
+    getUser = () => {
+        
     }
 
-    delete = () => {
-        this.db.pop()
-        console.log('DB atualizado')
-    }
+    // delete = () => {
+
+    // }
 }
 
