f59939ca8de9040bddd56dfe8c9f981502a245dc
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-15 08:49:27 -0300

    passando entidade que deve ser inicializada no database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,13 @@
 import { DataSource } from "typeorm"
+import { User } from "../entities/User"
 
 export const AppDataSource = new DataSource({
     //configracoes relacionanas ao banco de dados
     type: "sqlite",
     database: "./src/database/db.sqlite",
+    entities: [
+        User,
+    ],
     migrations: [
         "./src/database/migrations/*.ts"
     ]
