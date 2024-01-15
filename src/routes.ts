5c3d2fc1c50fec5d7514e4a011dd73b642a13341
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-15 08:46:01 -0300

    Corrigindo rota

--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,5 @@ export const router = Router()
 const userController = new UserController()
 
 router.post('/user', userController.createUser)
-router.get('/user', userController.getAllUsers)
+router.get('/user', userController.getUser)
 router.delete('/user', userController.delete)
